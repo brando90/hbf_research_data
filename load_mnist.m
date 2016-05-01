fb42b5b7ed83c18048d420d80f6594a1520444a6
Brando <brando90@mit.edu>
2016-05-01 16:42:39 -0400

    data

--- load_mnist.m
+++ load_mnist.m
@@ -1,5 +1,6 @@
-Xtrain = loadMNISTImages('train-images-idx3-ubyte');
-Ytrain = loadMNISTLabels('train-labels-idx1-ubyte')';
-Xtest = loadMNISTImages('t10k-images-idx3-ubyte');
-Ytest = loadMNISTLabels('t10k-labels-idx1-ubyte')';
+clear;clc;
+Xtrain = loadMNISTImages('train-images-idx3-ubyte')';
+Ytrain = loadMNISTLabels('train-labels-idx1-ubyte');
+Xtest = loadMNISTImages('t10k-images-idx3-ubyte')';
+Ytest = loadMNISTLabels('t10k-labels-idx1-ubyte');
 save('data_MNIST_original_minist_60k_10k_split_train_test')
