7092b6bb940f11e873bd338cec3040bc079d7ee6
Brando <brando90@mit.edu>
2016-05-01 17:28:42 -0400

    changed name

--- load_mnist.m
+++ load_mnist.m
@@ -1,6 +1,6 @@
 clear;clc;
-Xtrain = loadMNISTImages('train-images-idx3-ubyte')';
-Ytrain = loadMNISTLabels('train-labels-idx1-ubyte');
-Xtest = loadMNISTImages('t10k-images-idx3-ubyte')';
-Ytest = loadMNISTLabels('t10k-labels-idx1-ubyte');
+X_train = loadMNISTImages('train-images-idx3-ubyte')'; % (60,000 x 784)
+Y_train = loadMNISTLabels('train-labels-idx1-ubyte'); % (60,000 x 1)
+X_test = loadMNISTImages('t10k-images-idx3-ubyte')'; % (10,000 x 784)
+Y_test = loadMNISTLabels('t10k-labels-idx1-ubyte'); % (10,000 x 1)
 save('data_MNIST_original_minist_60k_10k_split_train_test')
